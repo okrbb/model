18d404794b683011fa9b241702e0dd36e8b8eccd
okrbb <123800197+okrbb@users.noreply.github.com>
2026-09-10 08:01:09 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -238,9 +238,10 @@ async function initModelerMap() {
         preferCanvas: true
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-    }).addTo(map);
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16
+}).addTo(map);
 
     fallbackMarkerLayerGroup = L.layerGroup().addTo(map);
     renderCityLabels();
